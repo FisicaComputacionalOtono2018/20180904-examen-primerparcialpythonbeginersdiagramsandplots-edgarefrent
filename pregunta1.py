59669ce657b810e7375fb0c5110c0804161b1019
edgarefrent <42181839+edgarefrent@users.noreply.github.com>
2018-09-04 11:47:40 -0500

    Add files via upload

---
+++ pregunta1.py
@@ -0,0 +1,9 @@
+#Edgar Efren Tomay Tiburcio
+#4 de septiembre de 2018
+#Programa que imprime los dos primeros numeros impares ni divisibles por 3, que sean menores a 7
+
+x=1
+while (x<=5):
+	if x%3!=0:
+		print(x)
+	x=x+2
